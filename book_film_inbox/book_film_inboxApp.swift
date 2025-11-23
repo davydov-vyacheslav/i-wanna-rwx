fb476ff12e58f557c563bacb35338e892eb9a884
Davydov V <vyacheslav.v.davydov@gmail.com>
2025-11-23 22:22:44 +0200

    Initial Commit

---
+++ book_film_inbox/book_film_inboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  book_film_inboxApp.swift
+//  book_film_inbox
+//
+//  Created by Slava Davydov on 23.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct book_film_inboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
